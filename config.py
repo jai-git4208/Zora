e2f3cc7d0770ec570e6c5fa8c604a583f3ca5853
jai-git4208 <jaiminpansal@gmail.com>
2025-02-09 10:08:18 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-class Config:
-    MODEL_NAME = "zora-transformer"
-    TRAIN_DATA_PATH = "data/train.json"
-    VAL_DATA_PATH = "data/val.json"
-    MODEL_SAVE_PATH = "models/zora_model.pt"
-    MAX_SEQ_LENGTH = 512
-    BATCH_SIZE = 8
-    LEARNING_RATE = 3e-5
-    EPOCHS = 5
